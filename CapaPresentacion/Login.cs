62694a6a03c31ed4d9a85e97d19e170fd160d9b0
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2024-04-01 01:03:45 -0300

    fist commit

---
+++ CapaPresentacion/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            Inicio form = new Inicio();
+            form.Show();
+            this.Hide();
+
+            form.FormClosing += frmClosing;
+        }
+
+        private void frmClosing(object sender,FormClosingEventArgs e)
+        {
+            this.txtDocumento.Text = "";
+            this.txtClave.Text = "";
+            this.Show();
+        }
+    }
+}
